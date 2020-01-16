21f78e3bafdda272addb3d2ea7e9a8c932bf3af3
Benjamin Vincent <luxcium@neb401.com>
2020-01-16 14:40:36 -0500

    to uppercase

---
+++ paths.sh
@@ -0,0 +1,21 @@
+[submodule "POWERLINE"]
+path = powerline
+url = https://github.com/Luxcium/powerline.git
+[submodule "powerlevel10k"]
+path = powerlevel10k
+url = https://github.com/Luxcium/powerlevel10k.git
+[submodule "ohmyzsh"]
+path = ohmyzsh
+url = https://github.com/Luxcium/ohmyzsh.git
+[submodule "custom-zsh"]
+path = custom-zsh
+url = https://github.com/Luxcium/custom-zsh.git
+[submodule "custom-tmux"]
+path = custom-tmux
+url = https://github.com/Luxcium/custom-tmux.git
+[submodule "python-repl"]
+path = python-repl
+url = https://github.com/Luxcium/python-repl.git
+[submodule "node-repl"]
+path = node-repl
+url = https://github.com/Luxcium/node-repl.git
