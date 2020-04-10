d0fd55a3dabcac1a6bc9021c25dd0ede81fe1c45
Benjamin Vincent <luxcium@neb401.com>
2020-04-10 10:27:49 -0400

    configuration updated for speed and efficiency
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

--- initial_load.zsh
+++ initial_load.zsh
@@ -1,23 +1,44 @@
-function __loader__() {
+function load_envs() {
     # Set you locale here
+    # strong set
+    # LANG="fr_CA.UTF-8"
+    # fall back
     : ${LANG:="fr_CA.UTF-8"}
     export LANG
 
+    : ${ENV_LOADED:="false"}
+    : ${SHOW_LOAD_CUTLS:="true"}
+    : ${VERBOSA:=0}
+
+}
+
+function __loader__() {
+    local PATH_FILE="${HOME}/.cache/path.env"
+    if [ -f "$PATH_FILE" ]; then
+        . $HOME/.cache/path.env
+        # local TIMER_NOW=$(/bin/date +%s%N)
+        local TIMER_NOW=$(/usr/local/bin/gdate +%s%N)
+        local TIMER_VALUE=$(((${TIMER_NOW} - ${TIMER_ALL_THEN}) / 1000000))
+        echo " ${TIMER_VALUE}   ms   to   'PATH'"
+    fi
+
+    # Loading the basic env variable to load and process the bootstraping
+    load_envs
+    # Other envs will be laoded later in the process
     local S1="${AHMYZSH}/paths.sh"
     if [ -f "$S1" ]; then
         . "${S1}"
         init_paths
     else
-        echo "Can not load 'init_paths' from '${S1}' please verify path or file"
+        [ "${VERBOSA}" -gt 3 ] && echo "Can not load 'init_paths' from '${S1}' please verify path or file"
     fi
 
-    export BASE_LAYOUTS="${ZSH_SOURCES}/layouts/base-layouts.sh"
-    local S1=${BASE_LAYOUTS}
+    local S1="${ZSH_LAYOUTS}/base-layouts.sh"
     if [ -f "$S1" ]; then
         . ${S1}
         load_layouts
     else
-        echo "Can not load 'load_layouts' from '${S1}' please verify path or file"
+        [ "${VERBOSA}" -gt 3 ] && echo "Can not load 'load_layouts' from '${S1}' please verify path or file"
     fi
 
     local S1="${AHMYZSH}/functions.sh"
@@ -25,6 +46,83 @@ function __loader__() {
         . "${S1}"
         init_functions
     else
-        echo "Can not load 'init_functions' from '${S1}' please verify path or file"
+        [ "${VERBOSA}" -gt 3 ] && echo "Can not load 'init_functions' from '${S1}' please verify path or file"
     fi
+
+    call_ my_envs
+
+    local S1="${CUSTOM_ZSH}/MAIN.zsh"
+    if [ -f "$S1" ]; then
+        . "${S1}"
+        source_all
+    else
+        [ "${VERBOSA}" -gt 3 ] && echo "Can not load 'source_all' from '${S1}' please verify path or file"
+    fi
+
+}
+
+function my_envs() {
+    ## fnm
+    export FNM_ALIASES="${HOME}/.fnm/aliases"
+    {
+        export NODEDEFAULT="${FNM_ALIASES}/default/bin"
+        export NODELATEST="${FNM_ALIASES}/latest/bin"
+        export NODELATEST_ARGON="${FNM_ALIASES}/latest-argon/bin"
+        export NODELATEST_BORON="${FNM_ALIASES}/latest-boron/bin"
+        export NODELATEST_CARBON="${FNM_ALIASES}/latest-carbon/bin"
+        export NODELATEST_DUBNIUM="${FNM_ALIASES}/latest-dubnium/bin"
+        export NODELATEST_ERBIUM="${FNM_ALIASES}/latest-erbium/bin"
+
+        alias node-default="${NODEDEFAULT}/node"
+        alias node-latest="${NODELATEST}/node"
+        alias node-argon="${NODELATEST_ARGON}/node"
+        alias node-boron="${NODELATEST_BORON}/node"
+        alias node-carbon="${NODELATEST_CARBON}/node"
+        alias node-dubnium="${NODELATEST_DUBNIUM}/node"
+        alias node-erbium="${NODELATEST_ERBIUM}/node"
+    }
+
+    export POWERLINE_BINDINGS="${POWERLINE}/powerline/bindings"
+
+    export PS2="$(tput setab 7)$(tput setaf 0)  "
+
+    # Ruby
+    export WITH_RBENV='true'
+    export PATH_RBENV='/Users/neb_401/.rbenv/shims'
+
+    # GEM
+    export WITH_BIN_GEM='true'
+    export PATH_BIN_GEM='/usr/local/lib/ruby/gems/2.6.0/bin:/usr/local/opt/ruby/bin'
+
+    export PERL5LIB="${HOME}/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"
+
+    export PERL_LOCAL_LIB_ROOT="${HOME}/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"
+    export PERL_MB_OPT='--install_base "${HOME}/perl5"'
+    export PERL_MM_OPT='INSTALL_BASE=/Users/neb_401/perl5'
+    export PATH_PKG_CONFIG='/usr/local/opt/sqlite/lib/pkgconfig'
+    # export PATH_PKG_CONFIG="/usr/local/opt/openssl/lib/pkgconfig"
+    export WITH_ANACONDA='true'
+    export PATH_ANACONDA3="${HOME}/anaconda3/tungsten/bin"
+
+    export LOAD_ONCE_00="true"
+
+    export NODE_DEPLOYEMENT_ENV='false'
+    export NODE_ENV='development'
+    export NODE_LOCAL_ENV='true'
+    export NODE_NO_READLINE=1
+    export NODE_REMOTE_ENV='true'
+    export NODE_REPL_HISTORY_SIZE=10000
+    export NODE_REPL_HISTORY="${HOME}/.node_repl_history"
+    export NODE_REPL_MODE='sloppy'
+    export NODE_TEST_ENV='false'
+    export NPM_CHECK_INSTALLER='pnpm'
+    export PYTHONSTARTUP="${HOME}/.pythonrc"
+
+    export GITHUB_TOKEN="${MAC_BOOK_PRO_ENV_REPO_GIST_HOOK_NOTIFICATIONS}"
+    export GITHUB_PASSWORD="${GITHUB_TOKEN}"
+    export GITHUB_USER='Luxcium'
+
+    export LOCALPORT=80
+    export PORT=80
+    export SERVER='dev-server'
 }
--- paths.sh
+++ paths.sh
@@ -1,12 +1,39 @@
-init_paths() {
+function personal_projects_paths() {
+    export LXIO="${HOME}/Developer/LuxciumProject/luxcium.io"
+    export PATH_INSDR_CODE_EXT="${HOME}/.vscode-insiders/extensions"
+    export PATH_LXIO_PRJ="${HOME}/Developer/LuxciumProject"
+    export PATH_LXIO="${PATH_LXIO_PRJ}/luxcium.io"
+    export PATH_OH_MY_ZSH="${ZSH}"
+    export PATH_QUESTRADE="${PATH_LXIO_PRJ}/questrade-ts"
+    export PATH_IEXCLOUD_API_WRAPPER="${PATH_LXIO_PRJ}/iexcloud-api-wrapper"
+    export PATH_IEX_API="${PATH_LXIO_PRJ}/iex-luxcium-api"
+
+    # /Users/neb_401/.vscode-insiders/extensions/dev-pop-n-lock-theme-vscode
+    export DEV_POPNLOCK="${PATH_INSDR_CODE_EXT}/dev-pop-n-lock-theme-vscode"
+}
+
+function init_paths() {
     if [ "$PARENT_ENV_LOADED" != 'true' ]; then
+
+        init_projects_paths
+        personal_projects_paths
+
         export CACHE_FOLDER="${HOME}/.cache/p10k-${USER}"
         export AHMYZSH="${HOME}/ahmyzsh"
-        # export NODE_REPL="${AHMYZSH}/node-repl"
+        export NODE_REPL="${AHMYZSH}/node-repl"
+        {
+            export NODE_REPL_SCRIPT="${NODE_REPL}/repl.js"
+        }
         export POWERLEVEL10K="${AHMYZSH}/powerlevel10k"
         # export PYTHON_REPL="${AHMYZSH}/python-repl"
         export CUSTOM_ZSH="${AHMYZSH}/custom-zsh"
         {
+            export PWRLN_BINDINGS="${POWERLINE}/powerline/bindings"
+
+            {
+
+            }
+
             export ZSH_SOURCES="${CUSTOM_ZSH}/sources"
             {
                 export ZSH_BIN="${CUSTOM_ZSH}/bin"
@@ -19,6 +46,11 @@ init_paths() {
                     export ZSH_COMPUTE="${ZSH_SOURCES}/compute"
                     export ZSH_FLAGS="${ZSH_SOURCES}/flags"
                     export ZSH_LAYOUTS="${ZSH_SOURCES}/layouts"
+                    export ZSH_LUXCIUM="${ZSH_SOURCES}"
+                    {
+                        export PATH_ZSH_FUNCTIONS="${ZSH_LUXCIUM}"
+                        export ZSH_FLAGS_VALUES="${ZSH_LUXCIUM}/flags/values"
+                    }
                 }
             }
             export OHMYZSH="${AHMYZSH}/ohmyzsh"
@@ -27,4 +59,31 @@ init_paths() {
             }
         }
     fi
+}
+
+function init_projects_paths() {
+
+    export AHMYZSH=${AHMYZSH}
+
+    export CUSTOM_TMUX="${AHMYZSH}/custom-tmux"
+    source "${CUSTOM_TMUX}/paths.sh"
+
+    export CUSTOM_ZSH="${AHMYZSH}/custom-zsh"
+    source "${CUSTOM_ZSH}/paths.sh"
+
+    export NODE_REPL="${AHMYZSH}/node-repl"
+    source "${NODE_REPL}/paths.sh"
+
+    export OHMYZSH="${AHMYZSH}/ohmyzsh"
+    source "${OHMYZSH}/paths.sh"
+
+    export POWERLEVLE10K="${AHMYZSH}/powerlevel10k"
+    source "${POWERLEVLE10K}/paths.sh"
+
+    export POWERLINE="${AHMYZSH}/powerline"
+    source "${POWERLINE}/paths.sh"
+
+    export PYTHON_REPL="${AHMYZSH}/python-repl"
+    source "${PYTHON_REPL}/paths.sh"
+
 }
