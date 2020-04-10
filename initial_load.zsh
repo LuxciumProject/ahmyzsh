bae14e39ba29ef1fef04bbea15b4f121ec5b3187
Benjamin Vincent <luxcium@neb401.com>
2020-04-10 01:30:35 -0400

    REMOVE CODE TEMPORARLY IN ORDER TO REWRITE CLEAN
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

--- initial_load.zsh
+++ initial_load.zsh
@@ -1,38 +1,30 @@
-function my_envs() {
-
-    : ${ENV_LOADED:="false"}
-    : ${SHOW_LOAD_CUTLS:="true"}
-    : ${VERBOSA:=1}
-    : ${EDITOR:='code'}
+function __loader__() {
+    # Set you locale here
+    : ${LANG:="fr_CA.UTF-8"}
+    export LANG
 
     local S1="${AHMYZSH}/paths.sh"
-    . "${S1}"
-    init_paths
+    if [ -f "$S1" ]; then
+        . "${S1}"
+        init_paths
+    else
+        echo "Can not load 'init_paths' from '${S1}' please verify path or file"
+    fi
 
     export BASE_LAYOUTS="${ZSH_SOURCES}/layouts/base-layouts.sh"
-    . ${BASE_LAYOUTS}
-    load_layouts
+    local S1=${BASE_LAYOUTS}
+    if [ -f "$S1" ]; then
+        . ${S1}
+        load_layouts
+    else
+        echo "Can not load 'load_layouts' from '${S1}' please verify path or file"
+    fi
 
     local S1="${AHMYZSH}/functions.sh"
-    . "${S1}"
-    init_functions
-
-}
-
-function lux_principium() {
-
-    my_envs
-    # reversed יְהִי אוֹר due to lack of support in my terminal
-    # [ "${VERBOSA}" -gt 0 ] && echo "${BEGIN_HOURGLASS_END_0} load_zshenv in $(timer_all) ms !${END_FUNCTION}"
-    #
-    # [ "${VERBOSA}" -gt 0 ] && echo "      \u001b[1m    רֹוא יִהְי \u001b[0m" # 'LET THERE BE LIGHT ...'
-    # https://en.wikipedia.org/wiki/Let_there_be_light
-    # 'LET THERE BE LIGHT ...'
-    [ "${VERBOSA}" -gt 0 ] && echo "${BEGIN_LBULB_ON}> $(timer_all) 'LET THERE BE LIGHT' ... ${END_SOURCING}"
-
-    local S1="${CUSTOM_ZSH}/MAIN.zsh"
-    . "${S1}"
-    source_all
-    source_ "${TMUX_FUNCTIONS_INDEX}"
-
+    if [ -f "$S1" ]; then
+        . "${S1}"
+        init_functions
+    else
+        echo "Can not load 'init_functions' from '${S1}' please verify path or file"
+    fi
 }
