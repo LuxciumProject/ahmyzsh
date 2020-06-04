f050af66434a7badcc775ee181f363f172d5bd9f
Benjamin <luxcium@neb401.com>
2020-06-04 16:13:57 -0400

    will move to fedora from wsl

--- initial_load.zsh
+++ initial_load.zsh
@@ -1,41 +1,164 @@
-LANG="fr_CA.UTF-8"
-LANGUAGE="fr_CA.UTßF-8"
-function my_envs() {
+# LANG="fr_CA.UTF-8"
+
+function load_envs() {
+    # Set log level here
+    : ${VERBOSA:=0}
+    export VERBOSA
+
+    # Set you locale here
+
+    # strong set
+    # LANG="fr_CA.UTF-8"
+    # LANGUAGE="fr_CA.UTF-8"
+
+    # fall back
+    : ${LANG:="fr_CA.UTF-8"}
+    export LANG
+    : ${LANGUAGE:="fr_CA.UTF-8"}
+    export LANGUAGE
     __LOCALE__
-    # This variable determines the locale category for any category not specifically selected via a variable starting # with ‘LC_’.
+
     : ${ENV_LOADED:="false"}
+    export ENV_LOADED
     : ${SHOW_LOAD_CUTLS:="true"}
-    : ${VERBOSA:=1}
-    : ${EDITOR:='code'}
+    export SHOW_LOAD_CUTLS
+    : ${PARENT_ENV_LOADED:="false"}
+    export PARENT_ENV_LOADED
+    : ${CONDA_PATH:="${HOME}/anaconda3/titanio"}
+    export CONDA_PATH
     : ${AHMYZSH:="$HOME/ahmyzsh"}
+    export AHMYZSH
+    : ${EDITOR:='code'}
+    export EDITOR
+
+    export separator1='\ue0bc'
+    export separator2='\ue0b8'
+    export separator3='\ue0c6'
+    export separator3r='\ue0c7'
+}
+
+function __loader__() {
+    local PATH_FILE="${HOME}/.cache/path.env"
+    if [ -f "$PATH_FILE" ]; then
+        . $HOME/.cache/path.env
+        local TIMER_NOW=$(/bin/date +%s%N)
+        # local TIMER_NOW=$(/usr/local/bin/gdate +%s%N)
+        local TIMER_VALUE=$(((${TIMER_NOW} - ${TIMER_ALL_THEN}) / 1000000))
+        echo -n " ${TIMER_VALUE}   ms   to   'PATH'"
+    fi
+
+    # Loading the basic env variable to load and process the bootstraping
+    load_envs
+    # Other envs will be laoded later in the process
 
     local S1="${AHMYZSH}/paths.sh"
-    . "${S1}"
-    init_paths
+    if [ -f "$S1" ]; then
+        . "${S1}"
+        personal_projects_paths
+        init_paths
+    else
+        [ "${VERBOSA}" -gt 3 ] && echo "Can not load 'init_paths' from '${S1}' please verify path or file"
+    fi
 
-    export BASE_LAYOUTS="${ZSH_SOURCES}/Layouts/base-layouts.sh"
-    . ${BASE_LAYOUTS}
-    load_layouts
+    local S1="${ZSH_LAYOUTS}/base-layouts.sh"
+    if [ -f "$S1" ]; then
+        . ${S1}
+        load_layouts
+    else
+        [ "${VERBOSA}" -gt 3 ] && echo "Can not load 'load_layouts' from '${S1}' please verify path or file"
+    fi
 
     local S1="${AHMYZSH}/functions.sh"
-    . "${S1}"
-    init_functions
+    if [ -f "$S1" ]; then
+        . "${S1}"
+        init_functions
+    else
+        [ "${VERBOSA}" -gt 3 ] && echo "Can not load 'init_functions' from '${S1}' please verify path or file"
+    fi
+
+    call_ init_projects_paths
+    call_ my_envs
+
+    local S1="${CUSTOM_ZSH}/MAIN.zsh"
+    if [ -f "$S1" ]; then
+        . "${S1}"
+        source_all
+    else
+        [ "${VERBOSA}" -gt 3 ] && echo "Can not load 'source_all' from '${S1}' please verify path or file"
+    fi
 
 }
 
-# reversed יְהִי אוֹר due to lack of support in my terminal
-# [ "${VERBOSA}" -gt 0 ] && echo "${BEGIN_HOURGLASS_END_0} load_zshenv in $(timer_all) ms !${END_FUNCTION}"
-# [ "${VERBOSA}" -gt 0 ] && echo "${BEGIN_LBULB_ON}> $(timer_all) 'LET THERE BE LIGHT' ... ${END_SOURCING}"
-# [ "${VERBOSA}" -gt 0 ] && echo "      \u001b[1m    רֹוא יִהְי \u001b[0m" # 'LET THERE BE LIGHT ...'
-# https://en.wikipedia.org/wiki/Let_there_be_light
-# 'LET THERE BE LIGHT ...'
-function lux_principium() {
-    my_envs
+function my_envs() {
+    ## fnm
+    export FNM_ALIASES="${HOME}/.fnm/aliases"
+    {
+        export NODEDEFAULT="${FNM_ALIASES}/default/bin"
+        export NODELATEST="${FNM_ALIASES}/latest/bin"
+        export NODELATEST_ARGON="${FNM_ALIASES}/latest-argon/bin"
+        export NODELATEST_BORON="${FNM_ALIASES}/latest-boron/bin"
+        export NODELATEST_CARBON="${FNM_ALIASES}/latest-carbon/bin"
+        export NODELATEST_DUBNIUM="${FNM_ALIASES}/latest-dubnium/bin"
+        export NODELATEST_ERBIUM="${FNM_ALIASES}/latest-erbium/bin"
 
-    local S1="${CUSTOM_ZSH}/MAIN.zsh"
-    . "${S1}"
-    source_all
-    reload_all_x
+        alias node-default="${NODEDEFAULT}/node"
+        alias node-latest="${NODELATEST}/node"
+        alias node-argon="${NODELATEST_ARGON}/node"
+        alias node-boron="${NODELATEST_BORON}/node"
+        alias node-carbon="${NODELATEST_CARBON}/node"
+        alias node-dubnium="${NODELATEST_DUBNIUM}/node"
+        alias node-erbium="${NODELATEST_ERBIUM}/node"
+    }
+
+    export POWERLINE_BINDINGS="${POWERLINE}/powerline/bindings"
+
+    export PS2="$(tput setab 7)$(tput setaf 0)  "
+
+    # Ruby
+    export WITH_RBENV='true'
+    export PATH_RBENV='/Users/neb_401/.rbenv/shims'
+
+    # GEM
+    export WITH_BIN_GEM='true'
+    export PATH_BIN_GEM='/usr/local/lib/ruby/gems/2.6.0/bin:/usr/local/opt/ruby/bin'
+
+    export PERL5LIB="${HOME}/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"
+
+    export PERL_LOCAL_LIB_ROOT="${HOME}/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"
+    export PERL_MB_OPT='--install_base "${HOME}/perl5"'
+    export PERL_MM_OPT='INSTALL_BASE=/Users/neb_401/perl5'
+    export PATH_PKG_CONFIG='/usr/local/opt/sqlite/lib/pkgconfig'
+    # export PATH_PKG_CONFIG="/usr/local/opt/openssl/lib/pkgconfig"
+    export WITH_ANACONDA='true'
+    export PATH_ANACONDA3="${HOME}/anaconda3/tungsten/bin"
+
+    export LOAD_ONCE_00="true"
+
+    export NODE_DEPLOYEMENT_ENV='false'
+    export NODE_ENV='development'
+    export NODE_LOCAL_ENV='true'
+    export NODE_NO_READLINE=1
+    export NODE_REMOTE_ENV='true'
+    export NODE_REPL_HISTORY_SIZE=10000
+    export NODE_REPL_HISTORY="${HOME}/.node_repl_history"
+    export NODE_REPL_MODE='sloppy'
+    export NODE_TEST_ENV='false'
+    export NPM_CHECK_INSTALLER='pnpm'
+    export PYTHONSTARTUP="${HOME}/.pythonrc"
+
+    export GITHUB_TOKEN="${MAC_BOOK_PRO_ENV_REPO_GIST_HOOK_NOTIFICATIONS}"
+    export GITHUB_PASSWORD="${GITHUB_TOKEN}"
+    export GITHUB_USER='Luxcium'
+
+    export LOCALPORT=80
+    export PORT=80
+    export SERVER='dev-server'
+}
+
+function lux_principium() {
+    __loader__
+
+    load_ reload_all_x
     source_ "${TMUX_FUNCTIONS_INDEX}"
 
 }
@@ -46,20 +169,52 @@ function __LOCALE__() {
     # LC_ALL="fr_CA.UTF-8"
 
     # This variable determines the locale category for character collation information within ranges in glob brackets and # for sorting.
-    LC_COLLATE="fr_CA.UTF-8"
+    LC_COLLATE="${LANGUAGE}"
 
     # This variable determines the locale category for character handling functions. If the MULTIBYTE option is in effect # this variable or LANG should contain a value that reflects the character set in use, even if it is a single-byte # character set, unless only the 7-bit subset (ASCII) is used. For example, if the character set is ISO-8859-1, a # suitable value might be en_US.iso88591 (certain Linux distributions) or en_US.ISO8859-1 (MacOS).
-    LC_CTYPE="fr_CA.UTF-8"
+    LC_CTYPE="${LANGUAGE}"
 
     # This variable determines the language in which messages should be written. Note that zsh does not use message # catalogs.
-    LC_MESSAGES="fr_CA.UTF-8"
+    LC_MESSAGES="${LANGUAGE}"
 
     # This variable affects the decimal point character and thousands separator character for the formatted input/output # functions and string conversion functions. Note that zsh ignores this setting when parsing floating point # mathematical expressions.
-    LC_NUMERIC="fr_CA.UTF-8"
+    LC_NUMERIC="${LANGUAGE}"
 
     # This variable determines the locale category for date and time formatting in prompt escape sequences.
-    LC_TIME="fr_CA.UTF-8"
+    LC_TIME="${LANGUAGE}"
 
-    LC_MONETARY="fr_CA.UTF-8"
+    LC_MONETARY="${LANGUAGE}"
 
+}
+
+into_luxcium_env() {
+
+}
+
+old_functions_pc-master-race() {
+
+    # function my_envs() {
+    #     # __LOCALE__
+    #     # This variable determines the locale category for any category not specifically selected via a variable starting # with ‘LC_’.
+
+    #     local S1="${AHMYZSH}/paths.sh"
+    #     . "${S1}"
+    #     init_paths
+
+    #     export BASE_LAYOUTS="${ZSH_SOURCES}/Layouts/base-layouts.sh"
+    #     . ${BASE_LAYOUTS}
+    #     load_layouts
+
+    #     local S1="${AHMYZSH}/functions.sh"
+    #     . "${S1}"
+    #     init_functions
+
+    # }
+
+    # reversed יְהִי אוֹר due to lack of support in my terminal
+    # [ "${VERBOSA}" -gt 0 ] && echo "${BEGIN_HOURGLASS_END_0} load_zshenv in $(timer_all) ms !${END_FUNCTION}"
+    # [ "${VERBOSA}" -gt 0 ] && echo "${BEGIN_LBULB_ON}> $(timer_all) 'LET THERE BE LIGHT' ... ${END_SOURCING}"
+    # [ "${VERBOSA}" -gt 0 ] && echo "      \u001b[1m    רֹוא יִהְי \u001b[0m" # 'LET THERE BE LIGHT ...'
+    # https://en.wikipedia.org/wiki/Let_there_be_light
+    # 'LET THERE BE LIGHT ...'
 }
--- paths.sh
+++ paths.sh
@@ -242,32 +242,95 @@ load_my_envs() {
 
 }
 
-##!!0###########################################################################
-##!!                                                                          ##
-#+!!         Copyright (c) 2019-present Benjamin Vincent Kasapoglu            ##
-#&!!                                                                          ##
-#&!!   This Source Code Form is subject to the terms of the Mozilla Public    ##
-#&!!   License, v. 2.0. If a copy of the MPL was not distributed with this    ##
-#&!!         file, You can obtain one at http://mozilla.org/MPL/2.0/.         ##
-#&!!                                                                          ##
-##!!   The above copyright notice and this license notice shall be included   ##
-##!!         in all copies or substantial portions of the Software.           ##
-##!!                                                                          ##
-##!!          ------------------------------------------------------          ##
-##!!                                                                          ##
-##!!    Disclaimer of Warranty                                                ##
-##!!   -------------------------                                              ##
-##!!                                                                          ##
-##!!   Covered Software is provided under this License on an "as is"          ##
-##!!   basis, without warranty of any kind, either expressed, implied, or     ##
-##!!   statutory, including, without limitation, warranties that the          ##
-##!!   Covered Software is free of defects, merchantable, fit for a           ##
-##!!   particular purpose or non-infringing. The entire risk as to the        ##
-##!!   quality and performance of the Covered Software is with You.           ##
-##!!   Should any Covered Software prove defective in any respect, You        ##
-##!!   (not any Contributor) assume the cost of any necessary servicing,      ##
-##!!   repair, or correction. This disclaimer of warranty constitutes an      ##
-##!!   essential part of this License. No use of any Covered Software is      ##
-##!!   authorized under this License except under this disclaimer.            ##
-##!!                                                                          ##
-##!!0###########################################################################
+path_into_luxcium_env() {
+    function personal_projects_paths() {
+        if [ "$PARENT_ENV_LOADED" != 'true' ]; then
+
+            export AHMYZSH="${HOME}/ahmyzsh"
+            export CACHE_FOLDER="${HOME}/.cache/p10k-${USER}"
+
+            export LXIO="${HOME}/Developer/LuxciumProject/luxcium.io"
+            export PATH_INSDR_CODE_EXT="${HOME}/.vscode-insiders/extensions"
+            export PATH_LXIO_PRJ="${HOME}/Developer/LuxciumProject"
+            export PATH_LXIO="${PATH_LXIO_PRJ}/luxcium.io"
+            export PATH_OH_MY_ZSH="${ZSH}"
+            export PATH_QUESTRADE="${PATH_LXIO_PRJ}/questrade-ts"
+            export PATH_IEXCLOUD_API_WRAPPER="${PATH_LXIO_PRJ}/iexcloud-api-wrapper"
+            export PATH_IEX_API="${PATH_LXIO_PRJ}/iex-luxcium-api"
+
+            # /Users/neb_401/.vscode-insiders/extensions/dev-pop-n-lock-theme-vscode
+            export DEV_POPNLOCK="${PATH_INSDR_CODE_EXT}/dev-pop-n-lock-theme-vscode"
+        fi
+    }
+
+    function init_paths() {
+        if [ "$PARENT_ENV_LOADED" != 'true' ]; then
+
+            export CUSTOM_TMUX="${AHMYZSH}/custom-tmux"
+            {
+                export TMUX_BIN="${CUSTOM_TMUX}/bin"
+                export TMUX_CONFIGS="${CUSTOM_TMUX}/configs"
+            }
+            export CUSTOM_ZSH="${AHMYZSH}/custom-zsh"
+            {
+                export ZSH_BIN="${CUSTOM_ZSH}/bin"
+                {
+                    export ZSH_BIN_ETC="${ZSH_BIN}/etc"
+                }
+
+                export ZSH_SOURCES="${CUSTOM_ZSH}/sources"
+                {
+                    export ZSH_COMPLETION="${ZSH_SOURCES}/completion"
+                    export ZSH_COMPUTE="${ZSH_SOURCES}/compute"
+                    export ZSH_FLAGS="${ZSH_SOURCES}/flags"
+                    export ZSH_LAYOUTS="${ZSH_SOURCES}/layouts"
+                    export ZSH_LUXCIUM="${ZSH_SOURCES}"
+                    {
+                        export PATH_ZSH_FUNCTIONS="${ZSH_LUXCIUM}"
+                        export ZSH_FLAGS_VALUES="${ZSH_LUXCIUM}/flags/values"
+                    }
+                }
+            }
+            export NODE_REPL="${AHMYZSH}/node-repl"
+            export OHMYZSH="${AHMYZSH}/ohmyzsh"
+            {
+                export ZSH="${OHMYZSH}"
+            }
+            export POWERLEVEL10K="${AHMYZSH}/powerlevel10k"
+            export PYTHON_REPL="${AHMYZSH}/python-repl"
+            export POWERLINE="${AHMYZSH}/powerline"
+            {
+                export POWERLINE_BINDINGS="${POWERLINE}/powerline/bindings"
+                export PWRLN_BINDINGS="${POWERLINE}/powerline/bindings"
+            }
+
+        fi
+
+    }
+
+    function init_projects_paths() {
+
+        local S1="${CUSTOM_TMUX}/paths.sh"
+        source_ ${S1}
+
+        local S1="${CUSTOM_ZSH}/paths.sh"
+        source_ ${S1}
+
+        local S1="${NODE_REPL}/paths.sh"
+        source_ ${S1}
+
+        local S1="${OHMYZSH}/paths.sh"
+        source_ ${S1}
+
+        local S1="${POWERLEVEL10K}/paths.sh"
+        source_ ${S1}
+
+        local S1="${POWERLINE}/paths.sh"
+        source_ ${S1}
+
+        local S1="${PYTHON_REPL}/paths.sh"
+        source_ ${S1}
+
+    }
+
+}
