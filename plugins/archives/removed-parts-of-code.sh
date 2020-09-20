821f52b85664fedd16cff52a7c9de3208935f9a6
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-09-20 10:51:30 -0400

    move archive to handl;e old stuff
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- plugins/archives/removed-parts-of-code.sh
+++
@@ -1,7 +0,0 @@
-# local S1="${AH_LIBRARIES}/load_intearctive_login_state.sh"
-# if [ -f "${S1}" ]; then
-#   . ${S1}
-#   Load_Intearctive_Login_State
-# else
-#   echo "Error loading '${S1}'... File or path can not be resolved"
-# fi
--- plugins/archives/save_stocks_symbols_and_ids.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/env zsh
-
-ts-node "/home/luxcium/development/luxcium.io/server/lib/tedis/seters/implementation/questrade-snp500.ts"
-ts-node "/home/luxcium/development/luxcium.io/server/lib/tedis/seters/implementation/questrade-iex.ts"
-ts-node "/home/luxcium/development/luxcium.io/server/lib/tedis/seters/implementation/iexlist.ts" &
-ts-node "/home/luxcium/development/luxcium.io/server/lib/tedis/seters/implementation/snp500list.ts" &
