aa5d18718c364b3da361eaa5e2c23684e69dd27c
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-09-30 04:13:27 -0400

    feat: new alliases
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- core/start-up-script.sh
+++ core/start-up-script.sh
@@ -6,4 +6,18 @@
 (sudo nice -n -10 ionice -c 1 -n 3 dnf clean all)
 (sudo nice -n -10 ionice -c 1 -n 3 dnf -4 makecache)
 (sudo nice -n 20 dnf upgrade --downloadonly -v -y) &
+
+# (
+#   cd /home/luxcium/src/v4l2loopback &&
+#     make clean &&
+#     git pull origin &&
+#     sudo make &&
+#     sudo make install &&
+#     sudo depmod -a -v &&
+#     sudo modprobe -v v4l2loopback &&
+#     cd /sys/devices/virtual/video4linux/
+# ) || echo "ERROR"
+
+# sudo wall -n "$(gphoto2 --abilities)"
+
 exit
