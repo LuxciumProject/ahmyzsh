37c8b2c5eb2fa7768f341565a3e865b959a4e2b7
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-11-29 11:41:09 -0500

    manage scripts
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- core/logout-script.sh
+++ core/logout-script.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+kquitapp5 plasmashell
+
 (fc-cache -v) &
 (sudo nice -n -10 ionice -c 1 -n 3 dnf clean all) &
 # sudo dnf remove -y xorg-x11-drv-amdgpu xorg-x11-drv-nouveau
--- core/start-up-script.sh
+++ core/start-up-script.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env sh
 
-# Start Up Script
+echo "Start Up Script begin"
 ( 
 
-    (code-insiders /home/luxcium/ahmyzsh) &
+    (nice -5 code-insiders /home/luxcium/ahmyzsh) &
 
-    (fc-cache -v) &
+    (nice -n 30 ionice -c 3 fc-cache -v) &
 
-    (fnm install latest-argon) &
-    (fnm install latest-boron) &
-    (fnm install latest-carbon) &
-    (fnm install latest-dubnium) &
-    (fnm install latest-erbium) &
-    (fnm install latest-fermium) &
-    (fnm install latest && fnm default 15 && fnm use default) &
+    (nice -n 30 ionice -c 3 fnm install 8) &
+    (nice -n 30 ionice -c 3 fnm install 10) &
+    (nice -n 30 ionice -c 3 fnm install 12) &
+    (nice -n 30 ionice -c 3 fnm install 14) &
+    (nice -n 30 ionice -c 3 fnm install 15 && fnm default 15 && fnm use default) &
 
-    (conda update conda -y && conda update --all -y) &
+    (nice -n 30 ionice -c 3 conda update conda -y && nice -n 30 ionice -c 3 conda update --all -y) &
 
     # (
     #     npm install -g concurrently@latest create-react-app@latest eslint@latest prettier@latest ts-node@latest vsce@latest yarn@latest npm@latest pnpm@latest typescript@latest
     # )&
 
-    (sudo nice -n -10 ionice -c 1 -n 3 dnf remove -y xorg-x11-drv-amdgpu xorg-x11-drv-nouveau) &
-    (sudo nice -n -10 ionice -c 1 -n 3 dnf -4 makecache) &
-    (sudo nice -n -10 dnf upgrade --downloadonly -v -y) &
+    (nice -n 30 ionice -c 3 dnf remove -y xorg-x11-drv-amdgpu xorg-x11-drv-nouveau) &
+    (nice -n 30 ionice -c 3 dnf -4 makecache) &
+    (nice -n 30 ionice -c 3 dnf upgrade --downloadonly -v -y) &
 
 )
+
+echo "Start Up Script end"
 
 exit
 
