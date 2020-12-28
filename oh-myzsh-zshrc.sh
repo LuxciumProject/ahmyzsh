9dacc36c02a972d99ece4d74e93e467963bd3103
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-12-28 09:40:13 -0500

    move back to actual OMZ master branch submodule
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- oh-myzsh-zshrc.sh
+++ oh-myzsh-zshrc.sh
@@ -2,7 +2,7 @@
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-export ZSH="/home/luxcium/ahmyzsh/ohmyzsh-2"
+export ZSH="/home/luxcium/ahmyzsh/ohmyzsh"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
