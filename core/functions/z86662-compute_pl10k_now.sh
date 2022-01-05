417e209de82da5c5405dce164d6145d9de6c1b69
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2022-01-05 14:06:38 -0500

    compute pl10k updates
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- core/functions/z86662-compute_pl10k_now.sh
+++ core/functions/z86662-compute_pl10k_now.sh
@@ -2,5 +2,5 @@ function compute_pl10K_now() {
     (env echo $(npm -v) >"${CACHE_FOLDER_}/NPM_VERSION.raw.txt")
     (env echo "$(cut -d ' ' -f 2 <<<$(tsc -v))" >"${CACHE_FOLDER_}/TSC_VERSION.raw.txt")
 
-    call_ "compute_pl10k"
+    call_ "compute_pl10k_"
 }
