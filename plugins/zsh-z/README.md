eeeb0fb61b0a3e692986d768b211ddbea7285aec
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-08-04 08:20:30 -0400

    feat: :beers: Aggregate commit.
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

---
+++ plugins/zsh-z/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d553b78f8cb2ae5bed759490f6c986006659076d88158cd1897c61942a4c2b87
+size 17592
---
+++ plugins/zsh-z/zsh-z.plugin.zsh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fa6017feb7ec5d6fff28614581743fa57690f05453b5ebe5b33e9189970c904d
+size 29862
