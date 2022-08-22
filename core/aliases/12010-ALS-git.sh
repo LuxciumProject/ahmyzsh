2ea0bfd6d79a2c96a5a600b807a6d7e4dd9d8bff
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-08-22 18:36:15 -0400

    fix: :bug: Remove bugs and error messages
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- core/aliases/12010-ALS-git.sh
+++ core/aliases/12010-ALS-git.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1535266e80647d79ca26a79a1cb90d78ad7df90c73f4ddb0b569d747943dc8a0
-size 11334
+oid sha256:b2ad3c44943157145bd02b2229053ea6f9d7b0c7f589ea9ce908c389967042c4
+size 11335
--- source-me-in-etc-zshenv.sh
+++ source-me-in-etc-zshenv.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c08230110e962972a59d905d13465b2e131bef939de44cc1fd0fa4165dda208
-size 5862
+oid sha256:471648d2b7d4c93da2826e54ff5605659de7621c08eb942470c2ee2c03ea9189
+size 5974
