05045e7ed0e15b22f3f794c7e1055271dcff9c74
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-09-24 14:32:13 -0400

    fix: :bug: Fix bug on cd to dir with .nvmrc file.
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- core/functions/01000-custom_cd.sh
+++ core/functions/01000-custom_cd.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c8cffc1c6ecb57a87e34e0165181cc008c7b1a3a9222998c5317f4acc1f0fb2
-size 966
+oid sha256:fbb4828b63a6a158224fd8d5fac73f339398ddfc0dd67ea93c97ec3331c53a68
+size 985
