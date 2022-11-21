8525b7f126b8f53915782785438d33da4eba89d8
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-11-21 01:02:00 -0500

    add paths to the list
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- core/paths/00000-init-paths.sh
+++ core/paths/00000-init-paths.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9547c64ef75b64d494734529fff1cc97a24e74a58937c5d0bc7f4ba6a23017f5
-size 1920
+oid sha256:5c074db73782101e8f72feaabbf1421ad9cfb37c86c60a20a2cba96cef205964
+size 1978
