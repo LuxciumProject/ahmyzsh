0b15bcd0a70224f2675934d7d0cfbe9e5da8299b
Benjamin Vincent <luxcium@neb401.com>
2022-12-20 11:21:24 -0500

    Update last changes.
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

--- core/paths/00000-init-paths.sh
+++ core/paths/00000-init-paths.sh
@@ -1,3 +1,80 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5c074db73782101e8f72feaabbf1421ad9cfb37c86c60a20a2cba96cef205964
-size 1978
+#!/usr/bin/env bash
+
+export AHMYZSH=${AHMYZSH:="${HOME}/ahmyzsh"}
+export AHMYZSH_CACHE=${AHMYZSH_CACHE:="${HOME}/.cache/ahmyzsh"}
+export CACHED_PATH=${CACHED_PATH:="${AHMYZSH_CACHE}/path.env"}
+export MAIN_BOOTSTRAP=${MAIN_BOOTSTRAP:="${AHMYZSH}/MAIN.sh"}
+
+export OHMYZSH="${AHMYZSH}/ohmyzsh"
+{
+  export ZSH="${OHMYZSH}"
+}
+export POWERLEVEL10K="${AHMYZSH}/powerlevel10k"
+export ZSH_PLUGINS="${AHMYZSH}/plugins"
+export ZSH_THEMES="${AHMYZSH}/themes"
+
+export AHMYZSH_CORE="${AHMYZSH}/core"
+export MYZSH_CORE="${AHMYZSH_CORE}"
+export ZSH_CORE="${AHMYZSH_CORE}"
+
+{
+  export CORE_ALIASES="${ZSH_CORE}/aliases"
+  export CORE_BIN="${ZSH_CORE}/bin"
+  export CORE_COMPLETE="${ZSH_CORE}/complete.d"
+  export CORE_COMPUTE="${ZSH_CORE}/compute-path"
+  export CORE_ENV="${ZSH_CORE}/env"
+  export CORE_FUNCTIONS="${ZSH_CORE}/functions"
+  export CORE_LAYOUTS="${ZSH_CORE}/layouts"
+  export CORE_OPTIONS="${ZSH_CORE}/options"
+  export CORE_PATHS="${ZSH_CORE}/paths"
+  export CORE_TEMP="${ZSH_CORE}/temp"
+}
+export DESTINATION_PATH="/media/luxcium/100 GIO/downloads"
+export MY_MULTIMEDIA="${AHMYZSH}/multimedia"
+{
+  export MY_SOUNDS="${MY_MULTIMEDIA}/sounds"
+}
+
+export CUSTOM_TMUX="${AHMYZSH}/tmux"
+. "${CUSTOM_TMUX}/paths.sh"
+# {
+#   # export ZSH_BIN="${CUSTOM_ZSH}/bin"
+#   {
+#     # export ZSH_BIN_ETC="${ZSH_BIN}/etc"
+#   }
+
+#   # export ZSH_SOURCES="${CUSTOM_ZSH}/sources"
+#   {
+#     # export ZSH_COMPLETION="${AHMYZSH_CORE}/complete.d"
+#     # export ZSH_FLAGS="${ZSH_SOURCES}/flags"
+#     # export ZSH_LAYOUTS="${ZSH_SOURCES}/layouts"
+#     # export ZSH_LUXCIUM="${ZSH_SOURCES}"
+#     {
+#       # export PATH_ZSH_FUNCTIONS="${ZSH_LUXCIUM}"
+#       # export ZSH_FLAGS_VALUES="${ZSH_LUXCIUM}/flags/values"
+#     }
+
+#   }
+# }
+
+# # /home/luxcium/ahmyzsh/core
+
+# # export CORE_FUNCTIONS="${AHMYZSH_CORE}/functions.1"
+# # export CORE_FUNCTIONS="${AHMYZSH_CORE}/functions.2"
+
+# {
+# export PLUGINS_BIN="${ZSH_PLUGINS}/bin"
+# }
+
+# ohmyzsh/
+# plugins/
+# powerlevel10k/
+# themes/
+
+# .git/
+# .vscode/
+# archives/
+# core/
+# crt-profiles/
+# node_modules/
+# templates/
--- tmux/paths.sh
+++ tmux/paths.sh
@@ -1,3 +1,18 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f8e464e282a2e00bc51e372f866cc55c84f1b3d8887a75a6af7f168d8f9d1c01
-size 753
+#!/usr/bin/bash
+export CUSTOM_TMUX="${AHMYZSH}/tmux"
+export CUSTOM_TMUX="${CUSTOM_TMUX}"
+export TMUX_LIB="${CUSTOM_TMUX}/lib"
+export TMUX_BIN="${CUSTOM_TMUX}/bin"
+
+export TMUX_LIB_FNCT="${TMUX_LIB}/functions"
+export TMUX_CONFIGS="${CUSTOM_TMUX}/configs"
+export TMUX_POWERLINE="${CUSTOM_TMUX}/powerline"
+export TMUX_FUNCTIONS="${CUSTOM_TMUX}/tmux-functions"
+export TMUX_LIB_FNCT_MAIN="${TMUX_LIB}/tmux-functions.sh"
+
+export TMUX_FUNCTIONS_INDEX="${TMUX_FUNCTIONS}/index.sh"
+export PWRLN_TMUX_CONF="${PWRLN_BINDINGS}/tmux/powerline.conf"
+export TMUX_LOADER="${TMUX_LIB}/tmux-loader.sh"
+export TMUX_COMMON_CONF="${TMUX_CONFIGS}/common.tmux.config"
+export TMUX_COMMON_THEME="${TMUX_CONFIGS}/theme.tmux.config"
+export TMUX_ALIASES="${CUSTOM_TMUX}/tmux-aliases.sh"
