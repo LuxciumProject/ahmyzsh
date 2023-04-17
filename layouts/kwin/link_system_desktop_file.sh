95f97577162f3356583dba137de3196b4aa3c41a
Luxcium <42672814+Luxcium@users.noreply.github.com>
2023-04-17 18:31:21 -0400

    Fix to the scripts
    Signed-off-by: Luxcium <42672814+Luxcium@users.noreply.github.com>

--- layouts/kwin/link_system_desktop_file.sh
+++ layouts/kwin/link_system_desktop_file.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Define the path to the applications folder
+target_apps_dir="/usr/share/applications/"
+
 # Get the path to the .desktop file as an argument
-desktop_file_path="$1"
+desktop_file_path="${1:-}"
+
+if [[ -z "$desktop_file_path" ]]; then
+  echo "Error: No file path specified."
+  exit 1
+fi
 
 # Check if the path is valid
 if [[ ! -f "$desktop_file_path" ]]; then
@@ -12,20 +24,21 @@ fi
 # Get the name of the .desktop file
 desktop_file_name="$(basename "$desktop_file_path")"
 
-# Define the path to the system-level applications folder
-system_apps_dir="/usr/share/applications/"
-
 # Check if the symlink already exists in the system-level applications folder
-if [[ -f "$system_apps_dir/$desktop_file_name" ]]; then
+echo "Checking if symlink exists at: $target_apps_dir$desktop_file_name"
+if [[ -L "$target_apps_dir$desktop_file_name" ]]; then
   echo -n "The symlink already exists. Do you want to override it? [y/N]: "
   read -r answer
   if [[ "$answer" == "y" ]]; then
     # Remove the existing symlink
-    sudo rm "$system_apps_dir/$desktop_file_name"
+    sudo rm "$target_apps_dir/$desktop_file_name"
   else
     exit 0
   fi
+elif [[ -e "$target_apps_dir$desktop_file_name" ]]; then
+  echo "Error: A file or directory with the name '$desktop_file_name' already exists in the system applications folder."
+  exit 1
 fi
 
 # Create the symlink in the system-level applications folder
-sudo ln -s "$desktop_file_path" "$system_apps_dir/$desktop_file_name"
+sudo ln -s "$desktop_file_path" "$target_apps_dir/$desktop_file_name"
